b3ff6c1accd7b48b1567b956bae642c2b1a4ae3e
Gazi Ehsanul Haque <antik.edu@gmail.com>
2024-10-14 00:19:24 +0600

    main

--- Project setup/redux/src/App.tsx
+++ Project setup/redux/src/App.tsx
@@ -6,7 +6,13 @@ function App() {
 
   return (
     <>
-      <h1 className='border border-red-500 m-3 '>React with redux</h1>
+    <div className='h-screen w-full flex justify-center items-center'>
+      <div className='border border-purple-300 flex p-10'>
+      <button className='bg-green-500 rounded-md px-3 py-2 text-xl font-semibold text-white'>Increment</button>
+      <h1 className='text-3xl mx-10'>0</h1>
+      <button className='bg-red-500 rounded-md px-3 py-2 text-xl font-semibold text-white'>Decrement</button>
+      </div>
+    </div>
     </>
   )
 }

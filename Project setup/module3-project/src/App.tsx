ffacdf5c0faff67cb1e7d9f6c7cb6ff045f661a5
Gazi Ehsanul Haque <antik.edu@gmail.com>
2024-10-16 12:17:54 +0600

    todo list

---
+++ Project setup/module3-project/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className="">MY TODO</h1>
+     <Button>Button</Button>
+    </>
+  )
+}
+
+export default App
